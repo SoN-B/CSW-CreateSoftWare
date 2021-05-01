1f22e85762bc4873710e4dfd7242d8de046c02ce
rinkis6 <71873822+rinkis6@users.noreply.github.com>
2021-05-01 15:45:52 +0900

    Update monster.h

--- main project/monster.h
+++ main project/monster.h
@@ -7,12 +7,14 @@ typedef struct character {
   int atk;
   int def;
   string ettype;
-  void init(int hp,int mp,int atk,int def,string ettype)
+  string enemykind;
+  void init(string enemykind,int hp = 5000,int mp = 1250 ,int atk = 500,int def=250,string ettype = normal)
   {
     this->hp = hp;
     this-> mp = mp;
     this-> atk = atk;
     this-> def = def;
     this-> ettype = ettype;
+    this-> enemykind = enemykind;
   }
 };
