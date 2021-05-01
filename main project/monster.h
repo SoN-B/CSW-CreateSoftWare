8f300c6a6249547571ab97ce5edb50793c97c7f3
rinkis6 <71873822+rinkis6@users.noreply.github.com>
2021-05-01 15:51:00 +0900

    Update monster.h

--- main project/monster.h
+++ main project/monster.h
@@ -9,7 +9,9 @@ typedef struct character {
   string ettype;
   string enemykind;
   void init(string enemykind,string ettype = normal,int hp = 5000,int mp = 1250 ,int atk = 500,int def=250)
-  {
+  {// character 슬라임 = {슬라임,normal,hp,mp,atk,def};
+    //character 슬라임_보스 = {슬라임,boss,hp,mp,atk,def};
+    //character 슬라임_엘리트 = {슬라임,elite,hp,mp,atk,def};
     this->hp = hp;
     this-> mp = mp;
     this-> atk = atk;
