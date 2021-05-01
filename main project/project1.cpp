c6e03a9a5199baa6f33d966e2346c27f97406cf7
rinkis6 <71873822+rinkis6@users.noreply.github.com>
2021-05-01 15:28:38 +0900

    Update project1.cpp

--- main project/project1.cpp
+++ main project/project1.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-struct SoNB {
+typedef struct SoNB {
 	public :
 		int hp = 10000;
 		int mp = 5000;
 		int atk = 300;
 		int def = 150;
-};
+}character;
 
 int main()
 {
