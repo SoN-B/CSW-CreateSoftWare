2c150dbc09af42702293926376d418fae2c41f01
SoN-B <78769618+SoN-B@users.noreply.github.com>
2021-05-03 23:29:51 +0900

    Update monster.h

--- main project/monster.h
+++ main project/monster.h
@@ -3,16 +3,14 @@
 #include <iostream> //+
 using namespace std;
 
-typedef struct mob {
+struct mob {
     int hp, mp, atk, def;
     string ettype; //몹종류 -> normal,elite,boss
     string kind; //종족
 
-    void init(string kind, int hp , int mp , int atk , int def , string ettype = "normal") 
-    //default 매개변수 맨끝
+    mob(string kind, int hp, int mp, int atk, int def, string ettype = "normal") //default 매개변수 맨끝
     {
-      //mob Slime_Normal = {"슬라임",hp,mp,atk,def,"normal"};
-      //Slime_Normal.init("slime",100,20,10,5,"normal");
+        //ex) mob Slime_Normal = { "slime",100,20,10,5,"normal" };
 
         this->kind = kind;
         this->hp = hp;
@@ -28,3 +26,4 @@ typedef struct mob {
         cout << "HP : " << hp << endl;
     }
 };
+
