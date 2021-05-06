4eafa662c62b6ad3c49eda76ee77220fcfb92c9c
choihb127 <80331750+choihb127@users.noreply.github.com>
2021-05-06 21:03:53 +0900

    Update project1.cpp

--- main project/project1.cpp
+++ main project/project1.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 #include "map.h"
+#include "invectory.h"
 using namespace std;
 
 int Menu;
@@ -18,32 +22,55 @@ struct My_Character { //기본
 	}
 };
 
+void atkmenu();//플레이어 공격메뉴-최현범
+void mobatk(struct mob);//몬스터 공격함수-최현범
+
 int main()
 {//메뉴 선택
 	My_Character SoNB;
+	vector<float>turn;//전투순서-최현범
 
 	do {
 		cout << "【 TEST BETA GAME 】\n\n" << "메뉴를 선택해 주세요 ~ !\n";
 		cout << "\n1.게임시작\n2.게임 설명\n3.게임 종료\n";
-		
+
 		cin >> Menu;
 		switch (Menu)
 		{
 		case 1:
-			for (int i = 0; i < map1.size(); i++) {
-				for (int j = 0; j < map1[i].size(); j++) {
-					if (SoNB.hp == 0) { //도중에 캐릭터가 사망시 게임오버
+			for (int i = 0; i < map1.size(); i++)
+			{
+				int monsternum = map1[i].size();//방에 있는 몬스터의 수-최현범
+				for (int j = 0; j < monsternum; j++)//몬스터 등장, 스피드 비교
+				{
+					cout << map1[i][j].kind << " 가 나타났다!" << endl;//몬스터 등장 출력
+					turn.push_back(map1[i][j].speed);
+				}
+				turn.push_back(SoNB.speed);
+				
+				while (true)//전투부분
+				{
+					if (SoNB.hp == 0) //도중에 캐릭터가 사망시 게임오버
+					{
 						cout << "Game over...";
 						i = map1.size(); //한번에 i를 끝인덱스까지 올려서 중첩for문 반복중지
 						break;
 					}
-
-					/*
-					여기에 전투 부분 구현
-					*/
+					sort(turn.begin(), turn.end());
+					for (int k = turn.size()-1; k >= 0; k--)//전투 순서에따라 전투하기
+					{
+						if(k==SoNB.speed)
+						{
+							atkmenu();
+						}
+						else if(k==map1[i][k].speed)
+						{
+							mobatk(map1[i][k]);
+						}
+					}
+					
 				}
 			}
-
 			if (!SoNB.hp == 0) cout << "Congratulations! Game clear!\n\n";
 			break;
 		case 2:
@@ -53,5 +80,6 @@ int main()
 			Flag = false;
 			break;
 		}
-	} while (Flag); //메뉴 정석(do while)
-}
+	} while (Flag); //메뉴 정석(do while) 
+
+} 
