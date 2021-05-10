339f1f61a72364fc79f219c07c2d0c53fa8c3143
choihb127 <80331750+choihb127@users.noreply.github.com>
2021-05-10 18:37:12 +0900

    Update project1.cpp
    플레이어 전투메뉴 추가

--- main project/project1.cpp
+++ main project/project1.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int Menu;
 bool Flag = true; //do while문 탈출조건
 
-struct My_Character { //기본
+typedef struct My_Character { //기본
 	float hp, mp, atk, def, speed;
 
 	My_Character(int hp = 200, int mp = 100, int atk = 50, int def = 30, int speed = 1.5)
@@ -20,16 +20,56 @@ struct My_Character { //기본
 		this->def = def;
 		this->speed = speed;
 	}
-};
+	
+	void Get_Character_Info()//내 상태 확인
+	{
+		cout << "HP : " << hp << "\nMP: " << mp << "\n공격력: " << atk << "\n방어력: " << def << endl;
+	}
+}My_Character;
 
-void atkmenu();//플레이어 공격메뉴-최현범
+void atkmenu(My_Character character, mob mob)//플레이어 공격 메뉴
+{
+	int choice;//선택
+	bool atkFlag = true;//공격함수제어
+	while (atkFlag)
+	{
+		cout << "무엇을 할까...\n1. 공격\n2. 스킬\n3. 가방\n4. 내상태\n5. 적 정보\n6. 도주" << endl; //메뉴
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			//공격
+			atkFlag = false;
+			break;
+		case 2:
+			//스킬
+			atkFlag = false;
+			break;
+		case 3:
+			//인벤토리
+			break;
+		case 4:
+			character.Get_Character_Info();
+			break;
+		case 5:
+			mob.GetInfo();
+			break;
+		case 6:
+			//도주
+			break;
+		default:
+			cout << "올바르지 않은 입력" << endl;
+			break;
+		}
+	}
+
+}
+
 void mobatk(struct mob);//몬스터 공격함수-최현범
 
 int main()
 {//메뉴 선택
 	My_Character SoNB;
-	vector<float>turn;//전투순서-최현범
-
 	do {
 		cout << "【 TEST BETA GAME 】\n\n" << "메뉴를 선택해 주세요 ~ !\n";
 		cout << "\n1.게임시작\n2.게임 설명\n3.게임 종료\n";
@@ -37,39 +77,31 @@ int main()
 		cin >> Menu;
 		switch (Menu)
 		{
-		case 1:
+		case 1://
 			for (int i = 0; i < map1.size(); i++)
 			{
-				int monsternum = map1[i].size();//방에 있는 몬스터의 수-최현범
-				for (int j = 0; j < monsternum; j++)//몬스터 등장, 스피드 비교
+				int leftmob = map1[i].size();//방에 남은 몬스터의 수
+				for (int j = 0; j < leftmob; j++)
 				{
-					cout << map1[i][j].kind << " 가 나타났다!" << endl;//몬스터 등장 출력
-					turn.push_back(map1[i][j].speed);
-				}
-				turn.push_back(SoNB.speed);
-				
-				while (true)//전투부분
-				{
-					if (SoNB.hp == 0) //도중에 캐릭터가 사망시 게임오버
+					cout << map1[i][j].kind << " 가 나타났다!" << endl;//몬스터 등장
+					if (SoNB.speed > map1[i][j].speed)//스피드 비교 
+					{
+						atkmenu(SoNB, map1[i][j]);//캐릭터 spd>mob spd 이므로 플레이어 선공
+						mobatk(map1[i][j], SoNB);
+					}
+					else
+					{
+						mobatk(map1[i][j], SoNB);//mob spd> 캐릭터 spd 이므로 mob 선공
+						atkmenu(SoNB, map1[i][j]);
+					}
+					if (SoNB.hp == 0) //도중에 캐릭터가 사망시 게임오버 (수정필요)
 					{
 						cout << "Game over...";
 						i = map1.size(); //한번에 i를 끝인덱스까지 올려서 중첩for문 반복중지
 						break;
 					}
-					sort(turn.begin(), turn.end());
-					for (int k = turn.size()-1; k >= 0; k--)//전투 순서에따라 전투하기
-					{
-						if(k==SoNB.speed)
-						{
-							atkmenu();
-						}
-						else if(k==map1[i][k].speed)
-						{
-							mobatk(map1[i][k]);
-						}
-					}
-					
 				}
+				
 			}
 			if (!SoNB.hp == 0) cout << "Congratulations! Game clear!\n\n";
 			break;
