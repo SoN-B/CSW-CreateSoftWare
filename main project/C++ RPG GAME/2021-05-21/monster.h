a09b9f6ab5020b71279e1b1c0f8ec58d8d241b93
SEOK <rinkis@naver.com>
2021-05-28 14:04:13 +0900

    [002nd] [Feat,Del] Exp,Money 함수 랜덤하게 추가되도록 설정. Monster에 Exp_Plus,  Money_Plus 삭제, 생성자에 Exp,Money칸 추가. Get_Info()함수에 Level칸 삭제.

--- main project/C++ RPG GAME/2021-05-21/monster.h
+++ main project/C++ RPG GAME/2021-05-21/monster.h
@@ -2,17 +2,16 @@
 #pragma once
 #include <string>
 #include <iostream>
+
 using namespace std;
 
 typedef struct Monster {
-    double Hp, Mp, Atk, Def, Speed, Exp = 0, Max_Exp = 100;//선공 선별(Speed)
-    double Money = 100;
-    double Level = 1;
+    double Hp, Mp, Atk, Def, Speed, Exp = 1;//선공 선별(Speed)
+    double Money;
     string Ettype; //몹종류 -> normal,elite,boss
     string Kind; //종족
-
     Monster(string Kind, double Hp = 0, double Mp = 0, double Atk = 0,
-        double Def = 0, double Speed = 1, string Ettype = "normal") //default 매개변수 맨끝
+        double Def = 0, double Speed = 1, double Exp = 1, double Money = 10; string Ettype = "normal") //default 매개변수 맨끝
     {
         //ex) Monster Slime_Normal = { "slime",100,20,10,5,1,"normal" };
 
@@ -23,12 +22,13 @@ typedef struct Monster {
         this->Def = Def;
         this->Speed = Speed;
         this->Ettype = Ettype;
+        this->Exp = Exp + rand() % 6 - 2.5;
+        this->Money = Money + rand() % 6 - 2.5;
     }
 
     void Get_Info()
     {
         cout << "\n종족 : " << this->Kind << " [" << Ettype << "] " << endl;
-        cout << "Level : " << this->Level << endl;
         cout << "HP : " << this->Hp << endl;
         cout << "MP : " << this->Mp << endl;
         cout << "ATK : " << this->Atk << endl;
@@ -54,24 +54,5 @@ typedef struct Monster {
     void Speed_Plus(double a)
     {
         this->Speed += a;
-    }
-    void Money_Plus(double a)
-    {
-        this->Money += a;
-    }
-    void Level_Up()
-    {
-        this->Level += 1;
-        this->Max_Exp = this->Max_Exp + (this->Max_Exp * 0.5);
-        this->Hp += 50;
-        this->Mp += 25;
-        this->Atk += 10;
-        this->Def += 5;
-        this->Speed += 0.1;
-        this->Exp = 0;
-    }
-    void Exp_Plus(double a)
-    {
-        this->Exp += a;
     }
 }Monster;
