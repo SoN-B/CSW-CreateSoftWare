9a644c15662832e9f92f805d194654f6cb953788
choihb127 <80331750+choihb127@users.noreply.github.com>
2021-06-04 22:29:50 +0900

    [Docs]  주석추가, 변수및 함수 이름변경

--- main project/C++ RPG GAME/2021-05-29/main.cpp
+++ main project/C++ RPG GAME/2021-05-29/main.cpp
@@ -9,22 +9,22 @@
 #include "map.h"
 using namespace std;
 
-int Menu;
-int Select;
-int Cancel;
-bool Firstflag = true; //do while문 탈출조건
-bool Secondflag = true;
-bool Thirdflag = true;
-bool Fourthflag = true;
+int Menu; //메인메뉴 선택 변수
+int Select; //Map_Combat 함수 선택 변수
+int Cancel; //게임설명 탈출용 변수 :0입력으로 탈출
+bool Mainflag = true; //main함수 do while문 제어 변수
+bool Gameflag = true; //do while문 내부 게임시작 제어 변수
+bool Storeflag = true;//상점 제어 변수
+//bool Fourthflag = true; //현재 사용안함
 
-void GoToXy(int x, int y)
+void GoToXy(int x, int y) //UI관련
 {
     COORD Pos;
     Pos.X = 2 * x;
     Pos.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
-void Dungeon()
+void Dungeon() //DUNGEON 로고
 {
     GoToXy(10, 10);
     cout << "DDDDD     UU       UU NNN      N	 GGGGGG    EEEEEEEE	OOOO	NNN      N";
@@ -42,7 +42,7 @@ void Dungeon()
     cout << "DDDDD         UUUU    NN     NNN	  GGGGG    EEEEEEEE	OOOO	NN     NNN";
     GoToXy(10, 17);
 }
-bool Menu1()
+bool Map_Combat()//맵선택 및 전투
 {
     system("cls");
     GoToXy(10, 15);
@@ -56,9 +56,9 @@ bool Menu1()
     Map = Maps[Select - 1];
     system("cls");
 
-    for (int i = 0; i < Map.size(); i++)
+    for (int i = 0; i < Map.size(); i++)//i=선택한 맵의 크기(방의 수)
     {
-        for (int j = 0; j < Map[i].size(); j++)
+        for (int j = 0; j < Map[i].size(); j++)//j=해당 방의 몬스터 수
         {
             cout << "------------------------\n";
             setColor(2);
@@ -70,9 +70,9 @@ bool Menu1()
 
             while (true)//전투
             {
-                if (SoNB_P->Speed > Map[i][j]->Speed)//스피드 비교 
+                if (SoNB_P->Speed > Map[i][j]->Speed)//스피드 비교: 플레이어선공
                 {
-                    Atk_Menu(SoNB_P, Map[i][j]);//캐릭터 spd>mob spd 이므로 플레이어 선공
+                    Atk_Menu(SoNB_P, Map[i][j]);//플레이어 턴(선공)
                     if (Map[i][j]->Hp < 0)//몬스터 사망시
                     {
                         cout << "\n" << Map[i][j]->Kind << " 를 물리쳤다!" << endl;
@@ -81,35 +81,35 @@ bool Menu1()
                         Map[i][j]->Mp = Temp_Mob_P->Mp;
                         DropItem = Drop_Item();
                         if (DropItem != "") Pick_Up_Item(Inventory, DropItem);
-                        break;
+                        break;//while문 탈출: 전투끝
                     }
-                    Mob_Atk(Map[i][j], SoNB_P);
+                    Mob_Atk(Map[i][j], SoNB_P);//몬스터 턴(후공)
                     if (SoNB_P->Hp < 0) //도중에 캐릭터가 사망시 게임오버 
                     {
                         cout << "Game over..." << endl;
                         SoNB_P->Restat();
-                        i = Map.size() - 1; //한번에 i를 끝인덱스까지 올려서 중첩for문 반복중지
-                        return false;
-                        break;
+                        i = Map.size() - 1; // 변수 i를 올려 for문 탈출
+                        return false; 
+                        break;//while문 탈출: 전투끝
                     }
                     cout << "------------------------\n";
 
                 }
 
-                else
+                else//플레이어의 spd가 mob보다 낮은 경우: 플레이어 후공
                 {
-                    Mob_Atk(Map[i][j], SoNB_P);//mob spd> 캐릭터 spd 이므로 mob 선공
-                    if (SoNB_P->Hp < 0)
+                    Mob_Atk(Map[i][j], SoNB_P);//몬스터 턴(선공)
+                    if (SoNB_P->Hp < 0) //도중에 캐릭터가 사망시 게임오버 
                     {
                         cout << "Game over..." << endl;
                         SoNB_P->Restat();
-                        i = Map.size() - 1;
-                        return false;
-                        break;
+                        i = Map.size() - 1; //변수 i를 올려 for문 탈출
+                        return false; 
+                        break;//while문 탈출: 전투끝
                     }
 
-                    Atk_Menu(SoNB_P, Map[i][j]);
-                    if (Map[i][j]->Hp < 0)
+                    Atk_Menu(SoNB_P, Map[i][j]);//플레이어 턴 (후공)
+                    if (Map[i][j]->Hp < 0) //몬스터 사망시
                     {
                         cout << Map[i][j]->Kind << " 를 물리쳤다!" << endl;
                         Monster_Die(SoNB_P, Map[i][j]);
@@ -117,15 +117,15 @@ bool Menu1()
                         Map[i][j]->Mp = Temp_Mob_P->Mp;
                         DropItem = Drop_Item();
                         if (DropItem != "") Pick_Up_Item(Inventory, DropItem);
-                        break;
+                        break;//while문 탈출: 전투끝
                     }
                     cout << "------------------------\n";
                 }
             }
         }
     }
-    if (SoNB_P->Hp > 0) cout << "\n\nDungeon clear!\n\n";
-    return true;
+    if (SoNB_P->Hp > 0) cout << "\n\nDungeon clear!\n\n"; //맵을 모두 클리어시 던전 클리어
+    return true; //던전클리어시 
 }
 
 int main()
@@ -156,8 +156,8 @@ int main()
         switch (Menu)
         {
         case 1:
-            Secondflag = true;//메인메뉴 --> 맵선택(flase방지)
-            while (Secondflag)
+            Gameflag = true;//메인메뉴 --> 맵선택(flase방지)
+            while (Gameflag)
             {
                 system("cls");
                 GoToXy(10, 15);
@@ -171,15 +171,15 @@ int main()
                 switch (Menu)
                 {
                 case 1:
-                    Secondflag = Menu1();
+                    Gameflag = Map_Combat();
                     break;
                 case 2:
-                    Thirdflag = true;
+                    Storeflag = true;
                     system("cls");
-                    while (Thirdflag) Thirdflag = Open_Store(Inventory);
+                    while (Storeflag) Storeflag = Open_Store(Inventory);
                     break;
                 case 3:
-                    Secondflag = false;
+                    Gameflag = false;
                     GoToXy(0, 30);
                     break;
                 default:
@@ -197,10 +197,10 @@ int main()
             }
             break;
         case 3:
-            Firstflag = false;
+            Mainflag = false;
             break;
         default:
             break;
         }
-    } while (Firstflag); //메뉴 정석(do while)
+    } while (Mainflag); //메뉴 정석(do while)
 }
