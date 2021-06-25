c7d3bb0da69cda222b10c73d07a621df01acf910
SEOK <rinkis@naver.com>
2021-06-26 01:27:45 +0900

    [Feat] Atk_Menu() 에 History_Reset() 추가.

--- main project/C++ RPG GAME/player.h
+++ main project/C++ RPG GAME/player.h
@@ -211,6 +211,7 @@ int Atk_Menu(My_Character* Character, Monster* Mob)//플레이어턴 메뉴
 		cout << "무엇을 할까...\n1. 공격\n2. 스킬\n3. 가방\n4. 퀘스트\n5. 도주" << endl; //기본 메뉴
 		Cursor_Pos_End();
 		Choice = _getch()-48;
+		History_Reset();
 		switch (Choice)
 		{
 		case 1: //기본공격
