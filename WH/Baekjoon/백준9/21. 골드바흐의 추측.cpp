347553a70286312106f0ce6423ebc307331ff421
user.pyola4981 <pyola4981@naver.com>
2021-10-30 14:56:27 +0900

    WH

--- WH/Baekjoon/백준9/21. 골드바흐의 추측.cpp
+++ WH/Baekjoon/백준9/21. 골드바흐의 추측.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int arr[1000001] = { 1,1,1,0, };
@@ -24,15 +24,16 @@ int main() {
 		if (left > right) cout << "Goldbach's conjecture is wrong.";
 		else cout << n << " = " << left << " + " << right << '\n';
 	}
-}
+}*/
 
-/*#include <iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
 int main() {
-	int n, temp;
+	int n;
 	vector<int> v;
+	vector<int> ans;
 
 	for (int i = 3; i <= 499997; i += 2) {
 		v.push_back(i);
@@ -45,25 +46,30 @@ int main() {
 	}
 
 	while (true) {
+		int temp = 0;
 		cin >> n;
 
 		if (n == 0) break;
 
 		else {
 			for (int i = 0; i < v.size(); i++) {
-				temp = n - v[i];
-				for (int j = v.size() - 1; j >= 0; j--) {
-					if (v[j] == temp) {
-						cout << n << " = " << v[i] << " + " << v[j] << "\n";
-						i = 500000;
+				ans.push_back(n - v[i]);
+				for (int j = 3; j * j <= ans.front(); j++) {
+					if (ans.front() % j == 0) {
+						ans.pop_back();
 						break;
 					}
 				}
-				if (i == v.size() -1) {
-					cout << "Goldbach's conjecture is wrong.";
+				if (!ans.empty()) {
+					cout << n << " = " << v[i] << " + " << ans.front() << "\n";
+					ans.clear();
+					temp = 1;
+					break;
 				}
 			}
 		}
+		if (temp == 0) {
+			cout << "Goldbach's conjecture is wrong.";
+		}
 	}
-}*/
-
+}
